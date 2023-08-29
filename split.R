86d263a71f8ba2df60dadb140db1c042a148f0bc
Augustyński Iwo <Iwo.Augustynski@IOS.local>
2023-08-29 16:48:14 +0200

    Added fuction to make a grid. Added time measure of st_split. Line 66 compare results.

--- split.R
+++ split.R
@@ -15,8 +15,6 @@ file.remove(file.path(poznan_pbf_filnename, "bbbike_Poznan.gpkg"))
 # osmextract::oe_vectortranslate(poznan_pbf_filnename)
 roads_all = osmextract::oe_get("Poznan", extra_tags = "maxspeed", force_vectortranslate = TRUE)
 
-pbox <- sf::st_bbox(roads_all)
-pbox <- data.frame(xmin = pbox[1], ymin = pbox[2], xmax = pbox[3], ymax = pbox[4])
 set.seed(1234)
 
 roads = roads_all |>
@@ -24,41 +22,46 @@ roads = roads_all |>
 
 time_st_segmentize = system.time({
   roads_segmented <- st_as_sf(roads, wkt = "geometry", crs = "EPSG:4326") |>
-  sf::st_segmentize(dfMaxLength = 5) |>  # roads consists of many segments of length 50 m max
-  sf::st_cast("LINESTRING") |>  # cast to LINESTRING
-  sf::st_collection_extract("LINESTRING") |>  # extract LINESTRING from MULTILINESTRING
+  sf::st_segmentize(dfMaxLength = 5)
 })
-waldo::compare(roads, roads_segmented)
-nrow(roads) / time_st_segmentize[3]
-nrow(roads_segmented) / nrow(roads)
 
 
-#### Grid
-lines_number <- 100
-x = seq.int(from = pbox$xmin, to = pbox$xmax, length.out = lines_number)
 
-# vertical lines
-grid_v <- as.data.frame(x) |>
-  mutate(y = x, from = pbox$ymin, to = pbox$ymax) |>
-  as.matrix()
-geom <- apply(grid_v, 1, function(x)  st_as_text(st_linestring(matrix(x, ncol = 2))), simplify = T)
-grid_v <- cbind(as.data.frame(grid_v), geom) |>  st_as_sf(wkt = "geom", crs = "EPSG:4326")
 
-# horizontal lines
-y = seq.int(from = pbox$ymin, to = pbox$ymax, length.out = lines_number)
-grid_h <- as.data.frame(y) |>
-  mutate(x = y, from = pbox$xmin, to = pbox$xmax) |> select(from, to, x, y) |>
-  as.matrix()
-geom <- apply(grid_h, 1, function(x)  st_as_text(st_linestring(matrix(x, ncol = 2))), simplify = T)
-grid_h <- cbind(as.data.frame(grid_h), geom) |>  st_as_sf(wkt = "geom", crs = "EPSG:4326")
+mkgrid <- function(x, lines_number = 100) {
+  # function creates grid based on the bbox of the sf object with defined number of lines
+  # inspired by https://grass.osgeo.org/grass82/manuals//v.mkgrid.html
+  lines_number <- 100
+  pbox <- sf::st_bbox(x)
+  pbox <- data.frame(xmin = pbox[1], ymin = pbox[2], xmax = pbox[3], ymax = pbox[4])
 
-# grid as one object
-grid <- st_union(rbind(grid_h, grid_v))
+  x = seq.int(from = pbox$xmin, to = pbox$xmax, length.out = lines_number)
+
+  # vertical lines
+  grid_v <- as.data.frame(x) |>
+    mutate(y = x, from = pbox$ymin, to = pbox$ymax) |>
+    as.matrix()
+  geom <- apply(grid_v, 1, function(x)  st_as_text(st_linestring(matrix(x, ncol = 2))), simplify = T)
+  grid_v <- cbind(as.data.frame(grid_v), geom) |>  st_as_sf(wkt = "geom", crs = "EPSG:4326")
+
+  # horizontal lines
+  y = seq.int(from = pbox$ymin, to = pbox$ymax, length.out = lines_number)
+  grid_h <- as.data.frame(y) |>
+    mutate(x = y, from = pbox$xmin, to = pbox$xmax) |> select(from, to, x, y) |>
+    as.matrix()
+  geom <- apply(grid_h, 1, function(x)  st_as_text(st_linestring(matrix(x, ncol = 2))), simplify = T)
+  grid_h <- cbind(as.data.frame(grid_h), geom) |>  st_as_sf(wkt = "geom", crs = "EPSG:4326")
+
+  # grid as one object
+  grid <- st_union(rbind(grid_h, grid_v))
+
+  list(grid = grid, grid_h = grid_h, grid_v = grid_v)
+}
 
 #### Verification
 plot(roads_all$geometry[1:5000], col = "red")
 plot(grid_v$geom, col = "lightgreen", add = T)
 plot(grid_h$geom, col = "lightgreen", add = T)
 
-
-
+grid <- mkgrid(roads_all)
+time_st_split <- system.time({roads_splitted <- lwgeom::st_split(roads, grid$grid)})
