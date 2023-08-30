bfb598e071d05b26fba9f35887902af6b47fb2bd
Augustyński Iwo <Iwo.Augustynski@IOS.local>
2023-08-30 18:33:55 +0200

    New fast function split_lines New grid option
    New visual verification

--- split.R
+++ split.R
@@ -50,9 +50,10 @@ set.seed(1234)
 roads = roads_all |>
   sample_n(100)
 
+##### 1. segmentize
 time_st_segmentize = system.time({
   roads_segmented <- st_as_sf(roads, wkt = "geometry", crs = "EPSG:4326") |>
-  sf::st_segmentize(dfMaxLength = 2)
+  sf::st_segmentize(dfMaxLength = 10)
 })
 
 #### Verification
@@ -61,18 +62,44 @@ time_st_segmentize = system.time({
 # plot(grid_h$geom, col = "lightgreen", add = T)
 
 grid <- mkgrid(roads_all)
-time_st_split <- system.time({roads_splitted <- lwgeom::st_split(roads, grid$grid)})
 
-rbind(time_st_segmentize, time_st_split)
+grd <- stars::st_as_stars(sf::st_bbox(roads_all), nx = 100, ny = 100, values = 1) |>
+  st_as_sf(as_points = FALSE) |> st_cast("MULTILINESTRING")
 
-##### Comparing results
-comp_results <- function(x, y){
-  x <- st_as_text(x$geometry) |> stringr::str_length()
-  y <- st_as_text(y$geometry) |> stringr::str_length()
-  z <- ifelse(x - y!=0 ,1,0)
-  sum(z)
+##### 2. st_split
+time_st_split <- system.time({roads_splitted <- lwgeom::st_split(tibble::rowid_to_column(roads), grid$grid)|>
+  sf::st_collection_extract("LINESTRING") |>
+  group_by(rowid) |> summarise(sum = mean(rowid)) |> st_cast("LINESTRING")})
+
+
+##### 3. split_lines
+split_lines <- function(roads, blades_h, blades_v) {
+  blades_h <- collapse::rsplit(blades_h, 1:nrow(blades_h)) # data.frame to list
+  blades_v <- collapse::rsplit(blades_v, 1:nrow(blades_v)) # data.frame to list
+  tmp <- purrr::map(blades_h,\(x) lwgeom::st_split(tibble::rowid_to_column(roads), x)) |> # split roads line by line
+    purrr::list_rbind() |> # resulting list to data.frame
+    sf::st_as_sf() |> # data.frame to sf object
+    sf::st_collection_extract("LINESTRING") |>
+    group_by(rowid) |> summarise(sum = mean(rowid)) |> st_cast("MULTILINESTRING") # splitted road segments back to one road
+
+  # the same but with vertical lines
+  purrr::map(blades_v,\(x) lwgeom::st_split(tmp, x)) |>
+    purrr::list_rbind() |> sf::st_as_sf() |> sf::st_collection_extract("LINESTRING") |>
+    group_by(rowid) |> summarise(sum = mean(rowid)) |> st_cast("MULTILINESTRING") # st_cast("LINESTRING") 'disappears' parts of multilines
 }
 
-# Number of transformed roads should be the same
-comp_results(roads, roads_segmented)
-comp_results(roads, roads_splitted)
+time_split_lines <- system.time({roads_split_lines <- split_lines(roads, grid$grid_h, grid$grid_v)})
+
+rbind(time_st_segmentize, time_st_split, time_split_lines)
+
+##### Visual verification
+
+library(tmap)
+tmap_mode("view")
+
+tm_shape(st_cast(roads_splitted$geometry[10], 'POINT')) + tm_dots(col = "red", scale = 4) +
+  tm_shape(st_cast(roads_split_lines$geometry[10], 'POINT')) + tm_dots(col = "blue", scale = 2) +
+  tm_shape(st_cast(roads$geometry[10], 'POINT')) + tm_dots(col = "green") +
+  tm_shape(grid$grid_v) + tm_lines(lty = "dotted") +
+  tm_shape(grid$grid_h) + tm_lines(lty = "dotted") +
+  tm_shape(grid$grid) + tm_lines()
